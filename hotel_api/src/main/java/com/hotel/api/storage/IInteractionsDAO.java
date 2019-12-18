0d3992024d172ab255e65ce258c83d26b4c7911a
derbergamotte <57452441+derbergamotte@users.noreply.github.com>
2019-12-18 14:33:21 +0300

    unload

--- hotel_api/src/main/java/com/hotel/api/storage/IInteractionsDAO.java
+++
@@ -1,9 +0,0 @@
-package com.hotel.api.storage;
-
-import java.util.List;
-import com.hotel.entities.Interactions;
-
-public interface IInteractionsDAO {
-	void addInteraction(Interactions interaction);
-	List<Interactions> listAllInteractions();
-}
--- hotel_api/src/main/java/com/hotel/api/storage/IRoomsDAO.java
+++
@@ -1,9 +0,0 @@
-package com.hotel.api.storage;
-
-import java.util.List;
-import com.hotel.entities.Rooms;
-
-public interface IRoomsDAO {
-	void addRoom(Rooms room);
-	List<Rooms> listAllRooms();
-}
